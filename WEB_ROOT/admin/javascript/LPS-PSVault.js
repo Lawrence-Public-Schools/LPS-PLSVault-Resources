4080b91610baa3870605a982c8e0e7d2ecf76834
BenHoule-LPS <benjamin.houle@lawrence.k12.ma.us>
2021-10-25 15:22:08 -0400

    Boostrap doesn't work on this page for some reason so custom building responsive classes, added png for VAT

--- WEB_ROOT/admin/javascript/LPS-PSVault.js
+++ WEB_ROOT/admin/javascript/LPS-PSVault.js
@@ -1,43 +1,34 @@
-function toggleExample() {
-  let ex = document.getElementById('exampleLog');
-  if (ex.style.display === "none") {
-    ex.style.display = "inline-block";
-  } else {
-    ex.style.display = "none";
-  }
-}
-
-function toggleVAT() {
-  let VAT = document.getElementById('lpsVAT');
-  if (VAT.style.display === "none") {
-    VAT.style.display = "inline-block";
-    document.getElementById('VATLink').innerHTML = "Vault Access Table <i>(Click to hide)</i>";
-  } else {
-    VAT.style.display = "none";
-    document.getElementById('VATLink').innerHTML = "Vault Access Table <i>(Click to show)</i>";
-  }
+if ($testURL.indexOf("admin/students/studentpages/plsvault_students.html") !== -1) {
+  $j(document).ready(AddLPSPSVaultResources);
+} else {
+  $j(document).ready(RemoveLPSPSVaultResources);
 }
 
 function AddLPSPSVaultResources() {
-   $j("div#LPS-PSVaultCustomhiddentable h2").insertBefore("#content-main > div.box-round")
-   $j("div#LPSPSVaultResources").insertBefore("#content-main > div.box-round")
-      $j('div#content-main > h2:first').each(function(){
+   $j("#LPS-PSVaultCustomhiddentable h2").insertBefore("#content-main > div.box-round")
+   $j("#LPSPSVaultResources").insertBefore("#content-main > div.box-round")
+      $j('#content-main > h2:first').each(function(){
       hideCollapseClasses($j(this));
       hideCollapseText($j(this));
       hideCollapseTarget($j(this));
    });
-   $j("#content-main > div.box-round").insertBefore("div#LPS-plsvault_students")
-   $j("div#LPS-PSVaultCustomhiddentable").remove();
+   $j("#content-main > div.box-round").insertBefore("#LPS-plsvault_students")
+   $j("#LPS-PSVaultCustomhiddentable").remove();
    $Vframe = '<div id="LPSPSVaulttarget"><iframe id="LPSPSVaultlog" src="/admin/students/studentpages/LPS-plsvault_students.html?frn='+$LPSPSVault+'" frameborder="0"></iframe></div>'
-   $j("div#LPSPSVaultResources").after($Vframe);
+   $j("#LPSPSVaultResources").after($Vframe);
 }
-
 function RemoveLPSPSVaultResources() {
-        $j("div#LPS-PSVaultCustomhiddentable").remove();
+        $j("#LPS-PSVaultCustomhiddentable").remove();
 }
 
-if ($testURL.indexOf("admin/students/studentpages/plsvault_students.html") !== -1) {
-        $j(document).ready(AddLPSPSVaultResources);
-} else {
-    $j(document).ready(RemoveLPSPSVaultResources);
+function toggleVAT() {
+  let $VAT = $j('#VAT');
+  let $link = $j('#VAT-Link')
+  if ($VAT.css("display") === "none") {
+    $VAT.css("display","inline-block");
+    $link.html("<i id='VAT-Link'>(click to hide)</i>");
+  } else {
+    $VAT.css("display","none");
+    $link.html("<i id='VAT-Link'>(click to show)</i>");
+  }
 }
